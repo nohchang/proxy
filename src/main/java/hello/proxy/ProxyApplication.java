308543d2e2ac2aa3f459f4a836dfcc14db9dd158
nohchang <nchq2123@naver.com>
2023-06-27 18:37:37 +0900

    예제

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,13 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({AppV1Config.class, AppV2Config.class})
+//스프링 부트 3.0 이상 | @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
